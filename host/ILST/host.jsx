75dfd1ff2bba90d1e64720e09c5b5d8f0c68c1e7
Tom Scharstein <trscharstein@gmail.com>
2020-08-11 10:58:34 -0700

    Trimming build

--- host/ILST/host.jsx
+++ host/ILST/host.jsx
@@ -1,1 +1,20 @@
-console.log('Host is online');
+console.log("Host is online");
+
+// Loading, executing, and unloading an action
+function runAction(name, string) {
+  // Create a temporary file
+  var tmp = File(Folder.desktop + "/temp.aia");
+  // Open it for writing and dump action string here
+  tmp.open("w");
+  tmp.write(string);
+  tmp.close();
+  // Load the action into Illustrator memory
+  app.loadAction(tmp);
+  // Execute the action:
+  // ( Name of Action, name of Set, show Dialogs )
+  app.doScript(name, "Live-Pathfinder", false);
+  // Remove the temporary file
+  tmp.remove();
+  // Remove the Action set from memory
+  app.unloadAction("Live-Pathfinder", "");
+}
